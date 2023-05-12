eb3ceaf94210a5baa30974b928391072eb5a6b2e
CR7123987 <127672459+CR7123987@users.noreply.github.com>
2023-05-13 00:51:36 +0530

    Update Dijkstra.java

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,1 +1,12 @@
+//Given a graph and a source vertex in the graph, find the shortest paths from the source to all vertices in the given graph.
 
+public class Dijkstra{
+  public static class Node implements Comparable<Node>{
+    public String name;
+    public int index;
+    public List<Node> neighbour= new ArrayList<>();
+    public int distance;
+    public HashMap<
+    
+  }
+}
